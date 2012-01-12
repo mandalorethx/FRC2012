ff25db124ed2c1eee54a19c9dba8978a65654927
Ryan <Ryan@Ryan-Laptop>
2012-01-12 17:01:37 -0500

    Added code to tell motors what to do.

--- src/edu/wpi/first/wpilibj/templates/FBW.java
+++ src/edu/wpi/first/wpilibj/templates/FBW.java
@@ -13,5 +13,25 @@ public class FBW {
     public FBW() {
         
     }
+    public double[] processVals(double yLeft, double yRight){
+                
+        if (yLeft < 0) {
+            yLeft = yLeft*yLeft*-1;
+        }
+        else{
+            yLeft = yLeft*yLeft;
+        }  
+        
+        if (yRight < 0) {
+            yRight = yRight*yRight*-1;
+        }
+        else{
+            yRight = yRight*yRight;
+        }
+        yLeft *= 0.9;
+        yRight *= 0.9;
+        double[] toMotors = {yLeft, yRight};
+        return toMotors;
+    }
     
 }
