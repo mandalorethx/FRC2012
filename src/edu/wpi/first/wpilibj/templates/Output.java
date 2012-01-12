c2b06512eaa6ec08456e587d930074814669c281
first1 <first1@ROCFIRSTXP12.blamericas.bausch.com>
2012-01-12 17:04:13 -0500

    Changed number of motors from 4 to 2. Might be changed back later - wjs

--- src/edu/wpi/first/wpilibj/templates/Output.java
+++ src/edu/wpi/first/wpilibj/templates/Output.java
@@ -12,29 +12,24 @@ import edu.wpi.first.wpilibj.can.CANTimeoutException;
  * @author Matt
  */
 public class Output {
-    public CANJaguar topLeft;
-    public CANJaguar topRight;
-    public CANJaguar bottomLeft;
-    public CANJaguar bottomRight;
+    
+    public CANJaguar left;
+    public CANJaguar right;
     
     public Output(){
         try{
-            this.topLeft = new CANJaguar(3);
-            this.topRight = new CANJaguar(4);
-            this.bottomLeft = new CANJaguar(5);
-            this.bottomRight = new CANJaguar(6);
+            this.left = new CANJaguar(3);
+            this.right = new CANJaguar(4);
         }catch(CANTimeoutException e){
             System.out.println("Could not initiate jaguars");
         }
        
     }
     
-    public void setMotors(double topLeft, double topRight, double bottomLeft, double bottomRight){
+    public void setMotors(double leftVal, double rightVal){
         try{
-            this.topLeft.setX(topLeft);
-            this.topRight.setX(topRight);
-            this.bottomLeft.setX(bottomLeft);
-            this.bottomRight.setX(bottomRight);
+            this.left.setX(leftVal);
+            this.right.setX(rightVal);
         }catch(CANTimeoutException e){
             System.out.println("Could not set motors");
         }
